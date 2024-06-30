520823e391836b6bfd9c592384e127ba8844b0be
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-06-30 13:05:15 -0300

    fix payload

--- internal/usecase/dto/payload.go
+++ internal/usecase/dto/payload.go
@@ -1,13 +1,7 @@
 package dto
 
-import (
-	"github.com/gorilla/websocket"
-)
-
 type Payload struct {
 	Message  string `json:"message"`
 	Username string `json:"username"`
-	Id       string
-	Conn     *websocket.Conn
 	Type     string `json:"type"`
 }
