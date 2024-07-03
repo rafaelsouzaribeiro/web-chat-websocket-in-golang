5d9b997eb8f15a6ccf75635dbad008a805225e04
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-07-02 21:06:47 -0300

    adding viper

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"log"
-	"os"
-	"strconv"
+	"github.com/spf13/viper"
 
 	"github.com/rafaelsouzaribeiro/web-chat-websocket-in-golang/internal/infra/web/websocket/server"
 )
 
 func main() {
 
-	appName := os.Getenv("APP_NAME")
-	wsEndpoint := os.Getenv("WS_ENDPOINT")
-	portStr := os.Getenv("PORT")
+	appName := viper.GetString("APP_NAME")
+	wsEndpoint := viper.GetString("WS_ENDPOINT")
+	portStr := viper.GetInt("PORT")
 
 	if appName == "" {
 		appName = "localhost"
@@ -22,16 +20,11 @@ func main() {
 		wsEndpoint = "/ws"
 	}
 
-	if portStr == "" {
-		portStr = "8080"
+	if portStr == 0 {
+		portStr = 8080
 	}
 
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		log.Fatalf("Invalid port: %v", err)
-	}
-
-	svc := server.NewServer(appName, wsEndpoint, port)
+	svc := server.NewServer(appName, wsEndpoint, portStr)
 	go svc.ServerWebsocket()
 	select {}
 
