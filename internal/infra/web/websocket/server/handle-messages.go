3da0c73ed0be7a0e5c109a8c45eb46f7a66b3605
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-07-05 22:02:36 -0300

    updating mutex

--- internal/infra/web/websocket/server/handle-messages.go
+++ internal/infra/web/websocket/server/handle-messages.go
@@ -4,16 +4,19 @@ import "fmt"
 
 func handleMessages() {
 	for msg := range broadcast {
-		mu.Lock()
-		for _, user := range users {
-			msg.Username = fmt.Sprintf("<strong>%s</strong>", msg.Username)
 
-			if err := user.conn.WriteJSON(msg); err != nil {
+		for _, user := range users {
+			mu.Lock()
+			msg.Username = fmt.Sprintf("<strong>%s</strong>", msg.Username)
+			err := user.conn.WriteJSON(msg)
+			mu.Unlock()
+
+			if err != nil {
 				fmt.Println("Error sending message:", err)
 				user.conn.Close()
 				deleteUserByUserName(user.username, false)
 			}
 		}
-		mu.Unlock()
+
 	}
 }
