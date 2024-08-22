d86b75cb487a7d376adb4dbaa753b0657964f10f
Rafael <rafaelribeirosouza86@gmail.com>
2024-08-22 16:47:21 -0300

    updating pagination

--- internal/infra/database/redis/repository/get-from-users-index.go
+++ internal/infra/database/redis/repository/get-from-users-index.go
@@ -3,12 +3,9 @@ package repository
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 
 	"github.com/rafaelsouzaribeiro/web-chat-websocket-in-golang/internal/entity"
 )
-
-var inter int64 = 0
 
 func (r *MesssageRepository) GetFromUsersIndex() (*[]entity.Message, error) {
 	ctx := context.Background()
@@ -25,10 +22,9 @@ func (r *MesssageRepository) GetFromUsersIndex() (*[]entity.Message, error) {
 	for _, msg := range messages {
 		var payload entity.Message
 		if err := json.Unmarshal([]byte(msg), &payload); err == nil {
-			payload.Username = fmt.Sprintf("%s,%d", payload.Username, inter)
 			payloads = append(payloads, payload)
 		}
-		inter++
+
 	}
 
 	return &payloads, nil
